e4954a4a692b37b12e7c6945a0c5b7cac0bfdcea
Hira Siddiqui <12besehsiddiqui@seecs.edu.pk>
2024-05-23 02:45:44 +0200

    feat: fetch web3 object at load time

--- src/plurality-modal/PluralityModal.js
+++ src/plurality-modal/PluralityModal.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Modal } from 'antd';
 import './modalBackground.css'
 
@@ -11,6 +11,7 @@ const PluralityModal = ({ isOpen, closePlurality, frameUrl, style }) => {
     const handleCancel = () => {
         closePlurality()
     };
+
 
     return (
         <>
