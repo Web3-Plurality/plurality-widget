ae48935225e56f080ea1c1c10f0875d83b0411f8
Mujtaba Idrees <mujtabaidrees@gmail.com>
2024-05-24 10:55:34 +0200

    feat: update baseUrl

--- src/plurality-modal/PluralitySocialConnect.js
+++ src/plurality-modal/PluralitySocialConnect.js
@@ -7,10 +7,10 @@ import { isDisabled } from '@testing-library/user-event/dist/utils';
 
 
 //for local development
- const baseUrl = "http://localhost:3000";
+ //const baseUrl = "http://localhost:3000";
 
 //for prod development
-//const baseUrl = "https://app.plurality.network";
+const baseUrl = "https://app.plurality.network";
 
 
 let frameUrl;
