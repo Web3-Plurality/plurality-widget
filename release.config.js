3c3f14c2a09c6e5bd35b62f9cc2995395ea21558
Mujtaba Idrees <mujtabaidrees@gmail.com>
2025-01-11 02:24:18 +0100

    feat: publish npm package via ci

--- release.config.js
+++ release.config.js
@@ -3,23 +3,11 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
     [
-      '@semantic-release/npm',
-      {
-        npmPublish: false, 
-      },
-    ],
-    [
-      '@semantic-release/exec',
-      {
-        prepareCmd: `
-          npm run webpack
-          docker build . --file Dockerfile --tag ghcr.io/web3-plurality/plurality-widget-demo-pre:latest &&
-          docker push ghcr.io/web3-plurality/plurality-widget-demo-pre:latest &&
-          docker tag ghcr.io/web3-plurality/plurality-widget-demo-pre:latest ghcr.io/web3-plurality/plurality-widget-demo-pre:\${nextRelease.version} &&
-          docker push ghcr.io/web3-plurality/plurality-widget-demo-pre:\${nextRelease.version}
-        `
-      },
+      '@semantic-release/exec', {
+        'prepareCmd': 'npm run build'
+      }      
     ],
     '@semantic-release/git',
     '@semantic-release/github'
